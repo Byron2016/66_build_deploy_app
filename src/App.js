ba459a3038d322403b0be1fbb6c7a05f7c433a97
MiPersona <bgva2005@yahoo.com>
2020-06-08 12:52:13 -0500

    0004_TraeDataAPI a Pantalla con Forma

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Output from './components/Output';
+import Select from './components/controls/Select';
+import Text from './components/controls/Text';
 import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      paras: 3,
+      paras: 2,
       html: true,
       text: '',
     }
@@ -32,12 +34,33 @@ class App extends Component {
 
   }
 
+  showHtml(x){
+    this.setState({html: x}, this.getSampleText);
+  }
+
+  changeParas(number){
+    this.setState({paras: number}, this.getSampleText);
+  }
+
   render() {
     return (
-      <div className="App">
-    <Output value = {
-      this.state.text
-    } />
+      <div className="App container">
+      <h1 className="text-center">ReactJS Sample Text Generator</h1>
+      <hr />
+      <form className="form-inline">
+        <div className="form-group">
+          <label>Paragrahps:</label>
+          <Text value={this.state.paras} onChange={this.changeParas.bind(this)} />
+        </div>
+        <div className="form-group">
+          <label>Include HTML:</label>
+          <Select value={this.state.html} onChange={this.showHtml.bind(this)} />
+        </div>
+      </form>
+      <br /><br />
+        <Output value = {
+          this.state.text
+        } />
 
     </div>
     );
