123f6efe7c4421430087005e85c0c9f396262e7e
MiPersona <bgva2005@yahoo.com>
2020-06-08 12:22:17 -0500

    0003_TraeDataAPI a pantalla

--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
 import React, { Component } from 'react';
 import './App.css';
+import Output from './components/Output';
 import axios from 'axios';
 
 class App extends Component {
-  constructor(props){
-  	super(props);
-  	this.state = {
-  		paras: 14,
-  		html: true,
-  		text: '',
-  	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      paras: 3,
+      html: true,
+      text: '',
+    }
   }
 
-  componentDidMount(){
-  	this.getSampleText();
+  componentDidMount() {
+    this.getSampleText();
   }
 
-  getSampleText(){
-  	// axios.get('http://hipsterjesus.com/api?paras='+this.state.paras+'&html='+this.state.html)
-  	axios.get('https://baconipsum.com/api/?type=all-meat&paras='+this.state.paras+'&type=hipster-centric&format='+this.state.html)
-  	//https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=html
-  	.then((response) => {
-  		this.setState({text: response.data}, function(){
-  			console.log(this.state);
-  		});
-  	})
-  	.catch((err) => {
-  		console.log(err);
-  	});
+  getSampleText() {
+    // axios.get('http://hipsterjesus.com/api?paras='+this.state.paras+'&html='+this.state.html)
+    axios.get('https://baconipsum.com/api/?type=all-meat&paras=' + this.state.paras + '&type=hipster-centric&format=' + this.state.html)
+      //https://baconipsum.com/api/?type=all-meat&paras=3&start-with-lorem=1&format=html
+      .then((response) => {
+        this.setState({ text: response.data }, function() {
+          console.log(this.state);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
   }
 
   render() {
-  	  return (
-    <div className="App">
-    hello
+    return (
+      <div className="App">
+    <Output value = {
+      this.state.text
+    } />
 
     </div>
-  );
+    );
   }
 }
 
